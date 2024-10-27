4cea8db63f3c51e547c47726c517c7d49e7abed2
Asmitapaudel <asmeetapdl@gmail.com>
2024-10-28 00:56:20 +0545

    basic of function and Hangman game

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,11 @@
+# Defining a new Function
+def helloWorld():
+    print("Hello")
+
+# Calling a Function
+helloWorld()
+
+def get_user_name():
+    name = input("Enter your name: ")
+    print("Your name is " + name)
+get_user_name()
