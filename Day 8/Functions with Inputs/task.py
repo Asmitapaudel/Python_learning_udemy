a9b3bb1410f4bf900c0bd2532e3157124a9950ae
Asmitapaudel <asmeetapdl@gmail.com>
2024-10-30 00:39:00 +0545

    caesar cipher: encode and decode a text

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,10 @@
+def greet():
+    print("Hello")
+    print(" Friends")
+    print("!")
+greet()
+
+def myfunction(input):
+    print(f"Hello {input}")
+name=input("Enter a name:")
+myfunction(name)
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,14 @@
 # Functions with input
-
-def greet_with_name(name):
-    print(f"Hello {name}")
+# Create a function with multiple inputs
+def greet_with_name(greeting,name):
+    print(f"{greeting} {name}")
     print(f"How do you do {name}?")
 
 
-greet_with_name("Jack Bauer")
+greet_with_name("Hello","Jack Bauer")
+
+
+# Call the greet_with() function using keyword arguments.
+def greet_with(greeting,name):
+    print(f"{greeting} {name}")
+greet_with(name="Jack",greeting="Hello")
